4c343144943d12bd689a10722c3e007f2f53001d
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-07-13 09:15:25 +0900

    후위표기식 미완성

--- kyungyeul/algorithm1/203/후위표기식/postfix.py
+++ kyungyeul/algorithm1/203/후위표기식/postfix.py
@@ -2,17 +2,20 @@ a = int(input())
 word = list(input())
 arr = []
 arr2 = []
-
+count = 0
 
 for i in range(a):
     arr.append(input())
 
 
-for i in range(len(word)):
-    if word[i].isupper():
-        arr2.append("대문")
-    else:
-        arr2.append("a")
+for i in word:
+    if i.isupper():
+        arr2.append(arr[count])
+        count += 1
+    elif i == "+" or "-" or "*" or "/":
+        arr2.append(i)
+
+print("".join(arr2))
 
 
-print(arr2)
+https://reakwon.tistory.com/62
