19ac0d113e24b635328c13054a4d89b904f5c457
monk <2012.monk@gmail.com>
2021-08-22 19:39:49 +0900

    DP 1로 만들기

--- 이성묵/DP/14631로만들기/main.py
+++ 이성묵/DP/14631로만들기/main.py
@@ -1,16 +1,14 @@
-cache = {0: 0, 1: 1, 2: 1, 3: 1}
+def solution(n):
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
 
-
-def solve(n):
-    if n in cache:
-        return cache[n]
-
-    i = solve(n // 2) + (n % 2) + 1
-    j = solve(n // 3) + (n % 3) + 1
-
-    cache[n] = min(i, j)
-    return cache[n]
+    return dp[n]
 
 
 if __name__ == '__main__':
-    print(solve(int(input())))
+    print(solution(int(input())))
