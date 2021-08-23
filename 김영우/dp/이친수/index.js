cf88bab0d02f8ea1ec51d852ed8c9c5f8570f73e
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-08-23 19:38:49 +0900

    code

--- 김영우/dp/이친수/index.js
+++ 김영우/dp/이친수/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `90`
+);
+ 
 
+const n = Number(stdin);
+//--------------------------------------------------------
+
+//let cnt = 0;
+// const DFS = (now, depth, n)=>{
+//     if(depth === n){
+//         cnt++;
+//         return;
+//     }
+//     if(now === 0) {
+//         DFS(0, depth + 1, n);
+//         DFS(1, depth + 1, n);
+//     }
+//     else{
+//         DFS(0, depth + 1, n);
+//     }
+//     return cnt;
+// }
+
+function solution(n){
+    let answer = 0;
+    //answer = DFS(0, 2, n);
+
+    let bynary = [BigInt(1), BigInt(1)];
+    for(let i=2;i<n;i++){
+        bynary.push(bynary[i-1] + bynary[i-2]);
+    }
+
+    answer = bynary[n-1].toString().replace('n', '');
+    return answer;
+}
+
+console.log(solution(n));
